0c7c3e5bae50a6ce79085727bd167a0320675c8f
TobyFlan <toby.flanagan@hotmail.co.uk>
2025-01-17 12:22:57 +0000

    fix package import route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/animations.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
