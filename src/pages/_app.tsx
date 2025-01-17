af9513f7030cdf8aca3a549fbae3d42ae695c412
TobyFlan <toby.flanagan@hotmail.co.uk>
2025-01-17 12:13:10 +0000

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import "@/styles/animations.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Toaster />
+      <Analytics />
     </>
   );
 }
